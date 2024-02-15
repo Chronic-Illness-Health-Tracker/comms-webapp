ba58e4d4cbbc50022b3568189c6769b95023f16a
JamieTremaine <jamie.tremaine@students.plymouth.ac.uk>
2024-02-15 21:14:40 +0000

    charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,91 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { RouterOutlet } from '@angular/router';
+import {
+    HelphiSidebarComponent,
+    UserSettingsMenuComponent,
+} from '@helphi/helphi-common-ui';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { ChartConfiguration, ChartData, ChartOptions, Color } from 'chart.js';
+import {
+    NgChartsConfiguration,
+    NgChartsModule,
+    ThemeService,
+} from 'ng2-charts';
+import { ChartsComponent } from './charts/charts.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [
+        RouterOutlet,
+        MatButtonModule,
+        MatDividerModule,
+        HelphiSidebarComponent,
+        UserSettingsMenuComponent,
+        NgbDropdownModule,
+        ChartsComponent,
+    ],
+    providers: [
+        { provide: NgChartsConfiguration, useValue: { generateColors: false } },
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
     title = 'comms-webapp';
+
+    private success = getComputedStyle(document.body).getPropertyValue(
+        '--bs-success'
+    );
+
+    private warning = getComputedStyle(document.body).getPropertyValue(
+        '--bs-warning'
+    );
+
+    private danger = getComputedStyle(document.body).getPropertyValue(
+        '--bs-danger'
+    );
+
+    private font = getComputedStyle(document.body).getPropertyValue(
+        '--bs-body-font-family'
+    );
+
+    private fontColour = getComputedStyle(document.body).getPropertyValue(
+        '--bs-body-color'
+    );
+
+    constructor(private themeService: ThemeService) {
+        const overrides: ChartOptions = {};
+    }
+
+    public chartOptions: ChartConfiguration['options'] = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'right',
+                labels: {
+                    boxWidth: 20,
+                    boxHeight: 20,
+                    color: this.fontColour,
+                    font: {
+                        family: this.font,
+                    },
+                    textAlign: 'left',
+                },
+            },
+        },
+    };
+
+    public pieChartData: ChartData<'pie', number[], string | string[]> = {
+        labels: ['Normal', 'Sub-clinical', 'Unwell'],
+        datasets: [
+            {
+                data: [300, 500, 100],
+                backgroundColor: [this.success, this.warning, this.danger],
+            },
+        ],
+    };
 }
---
+++ src/app/charts/charts.component.ts
@@ -0,0 +1,153 @@
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { viewport } from '@popperjs/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { NgChartsModule } from 'ng2-charts';
+
+@Component({
+    selector: 'app-charts',
+    standalone: true,
+    imports: [NgChartsModule],
+    templateUrl: './charts.component.html',
+    styleUrl: './charts.component.scss',
+})
+export class ChartsComponent implements OnInit {
+    @Input({ required: true }) data!: Array<number>;
+
+    private success = getComputedStyle(document.body).getPropertyValue(
+        '--bs-success'
+    );
+
+    private warning = getComputedStyle(document.body).getPropertyValue(
+        '--bs-warning'
+    );
+
+    private danger = getComputedStyle(document.body).getPropertyValue(
+        '--bs-danger'
+    );
+
+    private font = getComputedStyle(document.body).getPropertyValue(
+        '--bs-body-font-family'
+    );
+
+    private fontColour = getComputedStyle(document.body).getPropertyValue(
+        '--bs-body-color'
+    );
+
+    private breakpointMd = +getComputedStyle(document.body)
+        .getPropertyValue('--bs-breakpoint-md')
+        .replace('px', '');
+
+    private currentWidth: number;
+
+    protected prefferedSmallChart!: ChartType;
+    protected chartOneType: ChartType;
+    protected chartOneOptions: ChartConfiguration['options'];
+    protected chartData!: ChartData<ChartType, number[], string | string[]>;
+
+    constructor() {
+        this.currentWidth = window.innerWidth;
+        this.prefferedSmallChart = 'pie';
+
+        if (this.currentWidth < this.breakpointMd) {
+            this.chartOneType = this.prefferedSmallChart;
+            this.chartOneOptions = this.pieChartOptions;
+        } else {
+            this.chartOneType = 'pie';
+            this.chartOneOptions = this.pieChartOptions;
+        }
+    }
+
+    ngOnInit(): void {
+        this.chartData = {
+            labels: ['Normal', 'Sub-clinical', 'Unwell'],
+            datasets: [
+                {
+                    data: this.data,
+                    backgroundColor: [this.success, this.warning, this.danger],
+                },
+            ],
+        };
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event: { target: { innerWidth: number } }) {
+        this.currentWidth = event.target.innerWidth;
+
+        if (this.currentWidth >= this.breakpointMd) {
+            this.chartOneType = 'pie';
+            this.chartOneOptions = this.pieChartOptions;
+        } else {
+            this.chartOneType = this.prefferedSmallChart;
+            if (this.prefferedSmallChart === 'pie') {
+                this.chartOneOptions = this.pieChartOptions;
+            } else {
+                this.chartOneOptions = this.barChartOptions;
+            }
+        }
+    }
+
+    protected pieChartOptions: ChartConfiguration['options'] = {
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'right',
+                labels: {
+                    boxWidth: 20,
+                    boxHeight: 20,
+                    color: this.fontColour,
+                    font: {
+                        family: this.font,
+                    },
+                    textAlign: 'left',
+                },
+            },
+        },
+    };
+
+    protected barChartOptions: ChartConfiguration['options'] = {
+        scales: {
+            x: {
+                border: {
+                    width: 1,
+                    color: this.fontColour,
+                },
+                grid: {
+                    display: false,
+                },
+            },
+            y: {
+                border: {
+                    width: 1,
+                    color: this.fontColour,
+                },
+                grid: {
+                    display: false,
+                },
+            },
+        },
+        datasets: {
+            bar: {
+                barThickness: 50,
+            },
+        },
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    };
+
+    toggleChart() {
+        if (this.prefferedSmallChart === 'pie') {
+            this.chartOneOptions = this.barChartOptions;
+            this.prefferedSmallChart = 'bar';
+            this.chartOneType = 'bar';
+        } else {
+            this.chartOneOptions = this.pieChartOptions;
+            this.prefferedSmallChart = 'pie';
+            this.chartOneType = 'pie';
+        }
+    }
+}
