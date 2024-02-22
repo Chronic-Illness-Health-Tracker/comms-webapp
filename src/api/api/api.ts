efe6a2afcb612c12d1ca91f07d6b1c9b47bf9ed0
JamieTremaine <jamie.tremaine@students.plymouth.ac.uk>
2024-02-22 03:14:56 +0000

    added codegem

---
+++ src/api/api/api.ts
@@ -0,0 +1,13 @@
+export * from './gpController.service';
+import { GpControllerService } from './gpController.service';
+export * from './gpSurgeryController.service';
+import { GpSurgeryControllerService } from './gpSurgeryController.service';
+export * from './healthConditionController.service';
+import { HealthConditionControllerService } from './healthConditionController.service';
+export * from './organisationController.service';
+import { OrganisationControllerService } from './organisationController.service';
+export * from './patientController.service';
+import { PatientControllerService } from './patientController.service';
+export * from './userController.service';
+import { UserControllerService } from './userController.service';
+export const APIS = [GpControllerService, GpSurgeryControllerService, HealthConditionControllerService, OrganisationControllerService, PatientControllerService, UserControllerService];
---
+++ src/api/model/models.ts
@@ -0,0 +1,7 @@
+export * from './address';
+export * from './gp';
+export * from './gpSurgery';
+export * from './healthCondition';
+export * from './organisation';
+export * from './patient';
+export * from './user';
